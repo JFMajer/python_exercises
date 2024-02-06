ca3d11ea6f681f8c22f9eb51770035bd663d8e53
JFMajer <jakubmajer@live.com>
2024-02-06 20:15:28 +0100

    exercise 7, Ubbi Dubbi use list instead of string concat

--- ubbi_dubbi.py
+++ ubbi_dubbi.py
@@ -4,14 +4,14 @@ import string
 def ubbi_dubbi(word: string) -> string:
     word = word.lower()
     word = word.strip()
-    word_ubbi_dubbi = ""
+    word_ubbi_dubbi = []
     for l in word:
         if l in "aeiou":
-            word_ubbi_dubbi = word_ubbi_dubbi + "ub" + l
+            word_ubbi_dubbi.append(f'ub{l}')
         else:
-            word_ubbi_dubbi += l
+            word_ubbi_dubbi.append(l)
 
-    return word_ubbi_dubbi
+    return ''.join(word_ubbi_dubbi)
 
 
 ubi_dubi1 = ubbi_dubbi("program")
