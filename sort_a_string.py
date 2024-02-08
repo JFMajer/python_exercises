857598c99cf2c427bef532327c94185a9a0c5b60
JFMajer <jakubmajer@live.com>
2024-02-08 19:29:20 +0100

    exercise 8, additional exercise

--- sort_a_string.py
+++ sort_a_string.py
@@ -1,4 +1,13 @@
-def strsort(str1: str) -> str:
+def str_sort(str1: str) -> str:
     return ''.join(sorted(str1))
 
 
+str1 = str_sort("cuba")
+print(str1)
+
+def func2(str1: str) -> None:
+    splited = str1.split()
+    new_str = ', '.join(sorted(splited))
+    print(new_str)
+
+func2("Tom Bob Harry")
