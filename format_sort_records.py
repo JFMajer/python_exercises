cb8c802fb613f5f7bfc6a372941c64cb5c29c7e0
JFMajer <jakubmajer@live.com>
2024-02-29 20:00:21 +0100

    exercice 13, printing tuple records

--- format_sort_records.py
+++ format_sort_records.py
@@ -3,5 +3,13 @@ PEOPLE = [('Donald', 'Trump', 7.85),
           ('Jinping', 'Xi', 10.603)]
 
 
-def format_sort_records():
-    return None
+def format_sort_records(list_of_tuples):
+    list_of_tuples.sort(key = lambda a: a[1])
+    output = []
+    for p in list_of_tuples:
+        formatted = f"{p[1]:<10}{p[0]:<10}{p[2]:>5.2f}"
+        output.append(formatted)
+    return '\n'.join(output)
+
+
+print(format_sort_records(PEOPLE))
