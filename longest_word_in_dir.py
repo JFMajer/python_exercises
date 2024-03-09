70061485bd102cff3bc0bbeed403ad9b305dcb96
JFMajer <jakubmajer@live.com>
2024-03-09 14:31:20 +0100

    exercise 21, longest word in directory, more books added

--- longest_word_in_dir.py
+++ longest_word_in_dir.py
@@ -1,17 +1,24 @@
-import glob
 import pathlib
-from pathlib import Path
+import re
+
+
+def is_url(s):
+    # This is a simple pattern and might need to be adjusted based on the URLs you're encountering
+    pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+    return re.match(pattern, s) is not None
 
 
 def find_longest_word(filename: str) -> str | None:
-    path = Path(filename)
+    path = pathlib.Path(filename)
     longest_word = ""
 
     try:
+        # print(f"Opening file {path}")
         with path.open(mode='r') as file:
             for line in file:
-                for word in line.split():
-                    if len(word) > len(longest_word):
+                words = re.findall(r'\b\w+\b', line)
+                for word in words:
+                    if len(word) > len(longest_word) and not is_url(word):
                         longest_word = word
     except FileNotFoundError:
         print("Provided filename could not be found")
@@ -24,7 +31,7 @@ def find_all_longest_words(directory: str, pattern: str = '*.txt') -> dict:
     longest_words = {}
     files = pathlib.Path(directory).rglob(pattern)
     for f in files:
-        longest_words[str(f.name)] = find_longest_word(f)
+        longest_words[str(f.name)] = find_longest_word(str(f))
     return longest_words
 
 
