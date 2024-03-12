aa6aa44e31c2b4791101362575bb6daacfe5f3b5
JFMajer <jakubmajer@live.com>
2024-03-12 18:38:46 +0100

    first cli tool with Typer, better CLI arguments

--- first_cli/main.py
+++ first_cli/main.py
@@ -1,21 +1,23 @@
+from typing import Optional
+
 import typer
+from typing_extensions import Annotated
 from rich import print
-from rich.console import Console
-
-err_console = Console(stderr=True)
 
 
-def main(name: str, lastname: str = "", formal: bool = False):
+def main(name: Annotated[Optional[str], typer.Argument()] = None, lastname: Annotated[Optional[str], typer.Argument()] = None, formal: bool = False):
     """
     Say hi to NAME, optionally with a --lastname.
 
     If --formal is used, greets formally.
     """
-    err_console.print("Here is something written to standard error")
-    if formal:
-        print(f"Good day dearest sir/madam {name} {lastname}")
-    else:
-        print(f"Hello {name} {lastname}!")
+    greeting = "Good day sir/madam" if formal else "Hello"
+    full_name = f"{name or ''} {lastname or ''}".strip()
+
+    if not full_name:
+        print("Hello, what's your name?")
+        raise typer.Exit()
+    print(f"{greeting} {full_name}!")
 
 
 if __name__ == "__main__":
