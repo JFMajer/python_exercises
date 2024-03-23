f57b1f4497ea8ad2aed322ffd40e4a831b313c2f
JFMajer <jakubmajer@live.com>
2024-03-23 18:12:59 +0100

    exercise 27, password generator, closures, pep issues, function rename

--- password_generator.py
+++ password_generator.py
@@ -3,20 +3,20 @@ from typing import Callable
 
 
 def create_password_generator(characters: str) -> Callable[[int], str]:
-    def inner_function(length: int) -> str:
+    def create_password(length: int) -> str:
         random_chars = [random.choice(characters) for _ in range(length)]
         return ''.join(random_chars)
 
-    return inner_function
+    return create_password
 
 
 alpha_password = create_password_generator('abcdef')
 symbol_password = create_password_generator('!@#$%')
 print(alpha_password(5))
- # efeaa
+# efeaa
 print(alpha_password(10))
- # cacdacbada
+# cacdacbada
 print(symbol_password(5))
- # %#@%@
+# %#@%@
 print(symbol_password(10))
- # @!%%$%$%%#
+# @!%%$%$%%#
