508cee9bee8ec21224b3de9206138601d69c40fc
jakubmajer@live.com <jakubmajer@live.com>
2024-04-01 20:17:58 +0200

    new exception

--- freedonia/freedonia.py
+++ freedonia/freedonia.py
@@ -1,6 +1,9 @@
 class HourOutOfBound(Exception): pass
 
 
+class ProvinceDoesNotExists(Exception): pass
+
+
 provinces_tax_rate = {
     "Chico": 50,
     "Groucho": 70,
@@ -15,7 +18,7 @@ def calculate_tax(amount: float, province: str, time_of_purchase: int) -> float:
     if amount <= 0:
         raise ValueError("Purchase price needs to be more than zero.")
     if province.strip() not in provinces_tax_rate.keys():
-        raise ValueError(f"Province not found, please use one of {', '.join(provinces_tax_rate.keys())}")
+        raise ProvinceDoesNotExists(f"Province not found, please use one of {', '.join(provinces_tax_rate.keys())}")
 
     final_amount = amount + (amount * (provinces_tax_rate[province] / 100) * (time_of_purchase / 24))
     return round(final_amount, 3)
