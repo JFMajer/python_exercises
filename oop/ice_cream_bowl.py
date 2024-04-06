2f288c13f8f6b4e6d5da6f1f33aef3c2593bde5f
jakubmajer@live.com <jakubmajer@live.com>
2024-04-06 20:00:09 +0200

    ex 41, BigBowl

--- oop/ice_cream_bowl.py
+++ oop/ice_cream_bowl.py
@@ -7,15 +7,32 @@ from scoup import Scoop
 @dataclass
 class Bowl:
     scoops: List[Scoop] = field(default_factory=list)
+    bowl_limit: int = 3
 
     def add_scoops(self, *scoops: Scoop):
-        self.scoops.extend(scoops)
+        for s in scoops:
+            if len(self.scoops) < self.bowl_limit:
+                self.scoops.append(s)
+
+
+@dataclass
+class BigBowl(Bowl):
+    bowl_limit: int = 5
 
 
 s1 = Scoop('chocolate')
 s2 = Scoop('vanilla')
 s3 = Scoop('persimmon')
+s4 = Scoop("carrot")
+s5 = Scoop('flavor 5')
 b = Bowl()
 b.add_scoops(s1, s2)
 b.add_scoops(s3)
 print(b)
+b.add_scoops(s4)
+print(b)
+bb = BigBowl()
+bb.add_scoops(s1, s2)
+bb.add_scoops(s3)
+bb.add_scoops(s4, s5)
+print(bb)
