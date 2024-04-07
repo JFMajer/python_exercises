68b818702d7481caf7b36640b7ffa406715a2231
jakubmajer@live.com <jakubmajer@live.com>
2024-04-07 16:43:54 +0200

    ex 43, Animals

---
+++ oop/animals.py
@@ -0,0 +1,35 @@
+class Animal:
+    def __init__(self, species, number_of_legs, color):
+        self.species = species
+        self.number_of_legs = number_of_legs
+        self.color = color
+
+    def __str__(self):
+        return f'{self.color.capitalize()} {self.species}, {self.number_of_legs} legs'
+
+
+class Sheep(Animal):
+    def __init__(self, color):
+        super().__init__("sheep", 4, color)
+
+
+class Wolf(Animal):
+    def __init__(self, color):
+        super().__init__("wolf", 4, color)
+
+
+class Parrot(Animal):
+    def __init__(self, color):
+        super().__init__("parrot", 2, color)
+
+
+class Snake(Animal):
+    def __init__(self, color):
+        super().__init__("snake", 0, color)
+
+
+s = Sheep('white')
+print(s.species)
+print(s.color)
+print(s.number_of_legs)
+print(s)
